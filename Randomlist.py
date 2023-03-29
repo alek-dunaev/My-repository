cf3c9c4bb0f9da4c1bf93b14c9792a25cb52009d
alek-dunaev <122954102+alek-dunaev@users.noreply.github.com>
2023-03-29 10:11:06 +0500

    Create Randomlist.py
    Создание списка со случайными числами и поиск максимального элемента

---
+++ Randomlist.py
@@ -0,0 +1,14 @@
+import random
+randomlist = []
+for i in range(5):
+    m = random.randint(0,5)
+    randomlist.append(m)
+
+print(randomlist)
+
+nmax = 0;
+for i in randomlist:
+        if i > nmax:
+            nmax = i
+print(randomlist)
+print(nmax)
