09d7b21ec3eb83dbac6b0a1afaa8d45bcad90d7c
Alexander <122954102+alek-dunaev@users.noreply.github.com>
2023-06-09 12:31:57 +0500

    "Пример функции с комментариями и описанием типов переменных

---
+++ def_with_comments.py
@@ -0,0 +1,20 @@
+"""Пример функции с коментариями и описанием типов переменных"""
+
+
+def get_list_of_pets(self, auth_key: json, filter: str = "") -> json:
+        """Метод делает запрос к API сервера и возвращает статус запроса и результат в формате JSON
+        со списком наденных питомцев, совпадающих с фильтром. На данный момент фильтр может иметь
+        либо пустое значение - получить список всех питомцев, либо 'my_pets' - получить список
+        собственных питомцев"""
+
+        headers = {'auth_key': auth_key['key']}
+        filter = {'filter': filter}
+
+        res = requests.get(self.base_url + 'api/pets', headers=headers, params=filter)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
