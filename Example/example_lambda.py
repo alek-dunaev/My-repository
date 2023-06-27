4e6a592c74d284f6776401fa82a76f6c3b3c9dbf
Alexander <122954102+alek-dunaev@users.noreply.github.com>
2023-06-27 12:40:18 +0500

    Update example_lambda.py

--- Example/example_lambda.py
+++ Example/example_lambda.py
@@ -10,3 +10,13 @@ is_acceptable_password = lambda p: 0 < sum(c.isdigit() for c in p) < len(p) > 6
 
 print("Example:")
 print(is_acceptable_password("short"))
+
+
+
+
+"""In a given text you need to sum the numbers while excluding any digits that form part of a word.
+The text consists of numbers, spaces and letters from the English alphabet.
+Input: A string (str).
+Output: An integer (int)."""
+
+sum_numbers = lambda text: sum(int(word) for word in text.split() if word.isdigit())
